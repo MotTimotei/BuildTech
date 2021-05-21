27af1c01fd4e50a0848d88bfc14447f0f5cf8b17
Mot Timotei Fabian <73028729+MotTimotei@users.noreply.github.com>
2021-05-21 22:04:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,396 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <script type="text/javascript" src="js/classes/project.js"></script>
+    <title>Buildtech</title>
+  </head>
+  <body>
+    <header id="header_page" class="hdrE">
+      <div class="__logo">
+        <span class="bckImg">buildTech</span>
+      </div>
+      <div id="menu_op_cl">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav class="hedr_nv_p">
+        <ul class="he_nav_ul">
+          <li class="he_nav_li he_sub_nav_li upp" id="abUs"><a href="#">despre noi</a></li>
+          <li class="he_nav_li he_sub_nav_li upp" id="prtf"><a href="#">portofoliu</a></li>
+          <li class="he_nav_li he_sub_nav_li upp" id="weDo"><a href="#">ce facem</a></li>
+          <li class="he_nav_li he_sub_nav_li upp" id="cont"><a href="#">contacteaza-ne</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main id="main_page">
+      <div id="background">
+        <div class="inside_mn">
+          <h1>We build for you</h1>
+          <p>
+            Inventam, construim, inovam!<br>
+            Acoperim o arie extinsa in domeniul compatibilitatii programelor noastre.
+            Oferim servicii inovative pentru pagini web, site-uri, aplicatii web si de mobil, programe pentru desktop sau mac.<br>
+            Construim aplicatii pentru platforme IOS, Android si Windows.
+          </p>
+        </div>
+        <div class="prg_lng phone2">
+
+          <div id="l_1" class="lng_pr">
+            <div class="language">
+              <div id="lng_js" class="prg_lngghJ pasdhJ">
+                <span>JS</span>
+                <div class="code">
+                  <code id="var">const</code>
+                  <code id="variable">car1</code>
+                  <code id="operator">=</code>
+                  <code id="var">new</code>
+                  <code id="objec">car</code>
+                  <code id="sign">()</code>
+                  <code id="sign">;</code>
+                  <br>
+                  <br>
+                  <code id="var">function</code>
+                  <code id="function">getCar</code>
+                  <code id="sign">()</code>
+                  <code id="sign">{</code>
+                  <br>
+                  <br>
+                  <code id="sign">}</code>
+                </div>
+              </div>
+              <div id="lng_php" class="about_lng pasdhJ">
+                <span>PHP</span>
+                <div class="code">
+                  <code id="phptag"><</code>
+                  <code id="phptag">?php</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="phpkeys">foreach</code>
+                  <code id="phpsign">(</code>
+                  <code id="phpvar">$students</code>
+                  <code id="phpkeys">as</code>
+                  <code id="phpvar">$key</code>
+                  <code id="phpkeys">=></code>
+                  <code id="phpvar">$row</code>
+                  <code id="phpsign">)</code>
+                  <code id="phpsign">{</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <span id="code_break"></span>
+                  <code id="phpvar">$id</code>
+                  <code id="phpsign">[</code>
+                  <code id="phpvar">$key</code>
+                  <code id="phpsign">]</code>
+                  <code id="phpkeys">=</code>
+                  <code id="phpvar">$row</code>
+                  <code id="phpsign">[</code>
+                  <code id="phptext">'</code>
+                  <code id="phptext">id</code>
+                  <code id="phptext">'</code>
+                  <code id="phpsign">]</code>
+                  <code id="phpsign">;</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <span id="code_break"></span>
+                  <code id="phpvar">$first</code>
+                  <code id="phpsign">[</code>
+                  <code id="phpvar">$key</code>
+                  <code id="phpsign">]</code>
+                  <code id="phpkeys">=</code>
+                  <code id="phpvar">$row</code>
+                  <code id="phpsign">[</code>
+                  <code id="phptext">'</code>
+                  <code id="phptext">first</code>
+                  <code id="phptext">'</code>
+                  <code id="phpsign">]</code>
+                  <code id="phpsign">;</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <span id="code_break"></span>
+                  <code id="phpvar">$last</code>
+                  <code id="phpsign">[</code>
+                  <code id="phpvar">$key</code>
+                  <code id="phpsign">]</code>
+                  <code id="phpkeys">=</code>
+                  <code id="phpvar">$row</code>
+                  <code id="phpsign">[</code>
+                  <code id="phptext">'</code>
+                  <code id="phptext">last</code>
+                  <code id="phptext">'</code>
+                  <code id="phpsign">]</code>
+                  <code id="phpsign">;</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="phpsign">}</code>
+                  <br>
+                  <code id="phptag">?></code>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div id="l_2" class="lng_pr">
+            <div class="language">
+              <div id="lng_html" class="prg_lngghJ pasdhJ">
+                <span>HTML</span>
+                <div class="code">
+                  <code id="sign"><</code>
+                  <code id="tag">div</code>
+                  <code id="classes">class</code>
+                  <code id="sign">=</code>
+                  <code id="sign2">"</code>
+                  <code id="sign2">menu</code>
+                  <code id="sign2">"</code>
+                  <code id="sign">></code>
+                  <code id="sign"><</code>
+                  <code id="sign">/</code>
+                  <code id="tag">div</code>
+                  <code id="sign">></code>
+                </div>
+              </div>
+              <div id="lng_css" class="about_lng pasdhJ">
+                <span>CSS</span>
+                <div class="code">
+                  <code id="cssclass">.menu</code>
+                  <code id="cssoper">{</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="cssoper">width</code>
+                  <code id="cssoper">:</code>
+                  <code id="cssvalue">80%</code>
+                  <code id="cssoper">;</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="cssoper">height</code>
+                  <code id="cssoper">:</code>
+                  <code id="cssvalue">auto</code>
+                  <code id="cssoper">;</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="cssoper">margin</code>
+                  <code id="cssoper">:</code>
+                  <code id="cssvalue">0 auto</code>
+                  <code id="cssoper">;</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="cssoper">background-color</code>
+                  <code id="cssoper">:</code>
+                  <code id="cssvalue">#</code>
+                  <code id="cssvalue2">f2f2f2</code>
+                  <code id="cssoper">;</code>
+                  <br>
+                  <code id="cssoper">}</code>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="l_3" class="lng_pr">
+            <div class="language">
+              <div id="lng_cs" class="prg_lngghJ pasdhJ">
+                <span>C#</span>
+                <div class="code">
+                  <code id="creatStr">StringBuilder</code>
+                  <code id="csVar">message</code>
+                  <code id="csOper">=</code>
+                  <code id="function">new</code>
+                  <code id="creatStr">StringBuilder</code>
+                  <code id="csSign">()</code>
+                  <code id="csSign">;</code>
+                  <br>
+                  <code id="csVar">message</code>
+                  <code id="csSign">.</code>
+                  <code id="csVar">Append</code>
+                  <code id="csSign">(</code>
+                  <code id="csSign">"</code>
+                  <code id="csTxt">Welcome to buildTech!</code>
+                  <code id="csSign">"</code>
+                  <code id="csSign">)</code>
+                  <code id="csSign">;</code>
+                  <br>
+                  <code id="creatStr">Console</code>
+                  <code id="csSign">.</code>
+                  <code id="csVar">WriteLine</code>
+                  <code id="csSign">(</code>
+                  <code id="csVar">message</code>
+                  <code id="csSign">)</code>
+                  <code id="csSign">;</code>
+                </div>
+              </div>
+              <div id="lng_cpp" class="about_lng pasdhJ">
+                <span>c++</span>
+                <div class="code">
+                  <code id="cppvar">int</code>
+                  <code id="cppfunc">main</code>
+                  <code id="cppoper">()</code>
+                  <code id="cppoper">{</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="cppvar">string</code>
+                  <code id="cppoper">message</code>
+                  <code id="cppoper">;</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="cppoper">message</code>
+                  <code id="cppoper">=</code>
+                  <code id="cppch">"</code>
+                  <code id="cppch">Welcome</code>
+                  <code id="cppch">"</code>
+                  <code id="cppoper">;</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="cppoper">std::cout</code>
+                  <code id="cppoper"><<</code>
+                  <code id="cppoper">message</code>
+                  <code id="cppoper">;</code>
+                  <br>
+                  <span id="code_break"></span>
+                  <code id="cppret">return</code>
+                  <code id="cppnbr">0</code>
+                  <code id="cppoper">;</code>
+                  <br>
+                  <code id="cppoper">}</code>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="content">
+        <div class="pass_inv con_t_d" id="m1">
+          <div class="pass_info">
+            <div class="pass_invAh">
+              <h1>Pasiune pentru<br><span>inovare</span></h1><br>
+            </div>
+            <div class="pass_invAh2">
+              <p>
+                Folosim cea mai noua tehnologie pentru a fi mereu in tendinte. <br>
+                Utilizam cele mai noi limbaje de programare pentru a reda utilizatorului, continutul programului in cel mai bun mod posibil.
+               </p>
+               <button class="b_utt_on apsgdh __ajsH upp" type="button" name="button">incepe un proiect</button>
+            </div>
+          </div>
+          <div class="pass_vect phone2">
+            <div class="phone">
+              <div class="phoneframe">
+                <div class="phonescreen">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="portofolio con_t_d" id="m2">
+          <div class="pass_invAh">
+            <h1>Portofoliul <span>nostru</span></h1><br>
+          </div>
+          <div class="prj">
+            <button class="prev_project" id="prew" type="button" name="button">&#8249;</button>
+            <div class="projects"></div>
+            <button class="next_project" id='next' type="button" name="button">&#8250;</button>
+          </div>
+        </div>
+        <div class="prog_lang con_t_d" id="m3">
+          <div class="pass_invAh">
+            <h1>Ce tehnologie<br><span>folosim?</span></h1><br>
+          </div>
+          <div class="prg_lm_a">
+            <div class="lnGh">
+              <img src="img/c++.png" alt="c++">
+            </div>
+            <div class="lnGh">
+              <img src="img/cSh.png" alt="c#">
+            </div>
+            <div class="lnGh">
+              <img src="img/webDev.png" alt="HTML css javaScript">
+            </div>
+            <div class="lnGh">
+              <img src="img/nodeJs.png" alt="node.js">
+            </div>
+            <div class="lnGh">
+              <img src="img/java.png" alt="Java">
+            </div>
+            <div class="lnGh">
+              <img src="img/mysql.png" alt="mySQL">
+            </div>
+          </div>
+        </div>
+
+        <div class="portofolio con_t_d" id="m4">
+          <div class="pass_invAh">
+            <h1>Fa cunostinta<br><span>cu noi</span></h1><br>
+          </div>
+          <div class="meet_tm">
+            <div class="persoana per">
+              <div class="pers_front">
+                <div class="pers_front_body">
+                  <div class="pers_front_body_im">
+                    <img src="img/pers/timi.jpg">
+                  </div>
+                  <div class="pers_front_bdy_infobbl"></div>
+                  <div class="pers_front_bdy_info">
+                    <span class="pers_front_bdy_info_nm">Mot Timotei Fabian</span>
+                    <span class="pers_front_bdy_info_pos">CEO/Co Founder</span>
+                  </div>
+                </div>
+                <ul class="pers_front_footer1">
+                  <li id="pers_fb1" class="pers_front_footer_li1"><img src="img/facebook.png" alt=""> </li>
+                  <li id="pers_tw1" class="pers_front_footer_li1"><img src="img/twitter.png" alt=""> </li>
+                  <li id="pers_in1" class="pers_front_footer_li1"><img src="img/linkedin.png" alt=""> </li>
+                  <li id="pers_yt1" class="pers_front_footer_li1"><img src="img/youtube.png" alt=""> </li>
+                  <li id="pers_tt1" class="pers_front_footer_li1"><img src="img/twitch.png" alt=""> </li>
+                </ul>
+              </div>
+              <div class="pers_back"></div>
+          </div>
+        </div>
+      </div>
+      <div id="toTop" class="pos_fi-xed">
+        <span class="_arrow">
+          <span>&#x21bf;</span>
+          <span>&#x21be;</span>
+        </span>
+      </div>
+    </main>
+
+    <footer id="footer_page">
+
+    </footer>
+  </body>
+  <script type="text/javascript" src="js/moveProjects.js"></script>
+  <script type="text/javascript" src="js/menuNavigation.js"></script>
+  <script>
+    const totop = document.getElementById('toTop');
+    const hedr_nv_p = document.querySelector('.hedr_nv_p');
+    const hamb_button = document.getElementById('menu_op_cl');
+    const next_project = document.querySelector('.next_project');
+
+    let he_nav_li = document.querySelectorAll('.he_nav_li'), press = 0;
+
+    window.addEventListener('scroll', function(e) {
+      var scroll = this.scrollY;
+      if (scroll > window.innerHeight / 2) {
+        totop.style = "bottom:70px;transform:scale(1);";
+      } else {
+        totop.style = "bottom:-70px;transform:scale(.1);";
+      }
+    });
+
+    totop.addEventListener('click', function() {
+      window.scroll(0,0);
+    });
+
+    hamb_button.addEventListener('click', op_cl_menu);
+
+    function op_cl_menu() {
+  	   hamb_button.classList.toggle('open');
+       hedr_nv_p.classList.toggle('gHk_jk');
+
+  	}
+
+
+  </script>
+</html>
